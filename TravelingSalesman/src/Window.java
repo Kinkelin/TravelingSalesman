c29110e9fb505f7d59264a91e4cd12c220617d95
Finn Künkele <finn.kuenkele@nordakademie.de>
2018-06-03 18:45:32 +0200

    project added

---
+++ TravelingSalesman/src/Window.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+	public Window() {
+		setSize(1000, 1000);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args){ 
+        Window w = new Window();
+        
+    } 
+}
