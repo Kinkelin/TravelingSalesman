2a2a0c53c466df7054d8f65cff2d3a8b501f9a69
jonah iden <jonah.iden@gmail.com>
2018-06-26 13:56:29 +0200

    basics für CSV export eingebaut

---
+++ TravelingSalesman/src/misc/CSVFile.java
@@ -0,0 +1,39 @@
+package misc;
+
+import java.awt.List;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CSVFile {
+	
+	String path;
+	
+	StringBuilder builder;
+	
+	FileWriter writer;
+	
+	public CSVFile(String path) {
+		this.path = path;
+		builder = new StringBuilder();
+		
+		try {
+			writer = new FileWriter(path);
+		} catch (IOException e) {}
+	}
+	
+	public void writeLine(String[] data) {
+		for(String string: data) {
+			builder.append(string);
+			builder.append(";");
+		}
+		builder.append("\n");
+	}
+	
+	public void save() {
+		try {
+			writer.write(builder.toString());
+			writer.flush();
+			writer.close();
+		} catch(IOException e) {}
+	}
+}
